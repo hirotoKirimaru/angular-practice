f5d0bc9616fea89b65b2bda810d1dd5dc50da0fe
hirotoKirimaru <nainaistar@gmail.com>
2024-06-22 16:42:03 +0900

    biome format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, type OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import {
