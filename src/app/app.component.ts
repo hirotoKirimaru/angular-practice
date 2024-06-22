94835c08c97cf65f8e678d0f8e087be64fd7c0b7
hirotoKirimaru <nainaistar@gmail.com>
2024-06-22 16:41:18 +0900

    biome format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,72 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {debounceTime, distinctUntilChanged} from "rxjs";
+import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import {
+	FormControl,
+	FormGroup,
+	FormsModule,
+	ReactiveFormsModule,
+	Validators,
+} from "@angular/forms";
+import { debounceTime, distinctUntilChanged } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent implements OnInit {
-  title = 'front';
-  public emailControl: FormControl = new FormControl("", [Validators.required, Validators.email, this.domainValidator]);
-  public nameOrEmailControl: FormControl = new FormControl("", [Validators.required]);
+	title = "front";
+	public emailControl: FormControl = new FormControl("", [
+		Validators.required,
+		Validators.email,
+		this.domainValidator,
+	]);
+	public nameOrEmailControl: FormControl = new FormControl("", [
+		Validators.required,
+	]);
 
-  ngOnInit() {
-    this.emailControl.statusChanges.subscribe((status) => {
-      if (status === 'VALID') {
-        console.log("Form changed:", this.emailControl.value);
-      }
-    })
+	ngOnInit() {
+		this.emailControl.statusChanges.subscribe((status) => {
+			if (status === "VALID") {
+				console.log("Form changed:", this.emailControl.value);
+			}
+		});
 
-    this.nameOrEmailControl.valueChanges.pipe(
-      debounceTime(1000), // 1秒間の待機
-      distinctUntilChanged() // 値が実際に変更された場合のみ発火
-    ).subscribe(value => {
-      if (true) {
-        console.log("value:", value);
-        console.log(this.nameOrEmailControl.valid);
-        // 後からValidatorsを変更する場合は、updateValueAndValidityを呼ぶ必要がある
-        // 初回だけTRUE, FALSEとなっている
-        this.nameOrEmailControl.setValidators([Validators.required, Validators.email])
-        // this.nameOrEmailControl.updateValueAndValidity()
-        this.nameOrEmailControl.updateValueAndValidity({emitEvent: false});
+		this.nameOrEmailControl.valueChanges
+			.pipe(
+				debounceTime(1000), // 1秒間の待機
+				distinctUntilChanged(), // 値が実際に変更された場合のみ発火
+			)
+			.subscribe((value) => {
+				if (true) {
+					console.log("value:", value);
+					console.log(this.nameOrEmailControl.valid);
+					// 後からValidatorsを変更する場合は、updateValueAndValidityを呼ぶ必要がある
+					// 初回だけTRUE, FALSEとなっている
+					this.nameOrEmailControl.setValidators([
+						Validators.required,
+						Validators.email,
+					]);
+					// this.nameOrEmailControl.updateValueAndValidity()
+					this.nameOrEmailControl.updateValueAndValidity({ emitEvent: false });
 
-        console.log(this.nameOrEmailControl.valid);
-      }
-    })
-  }
+					console.log(this.nameOrEmailControl.valid);
+				}
+			});
+	}
 
-  domainValidator(control: FormControl): { [key: string]: any } | null {
-    const email = control.value;
-    if (email && email.indexOf('@') !== -1) {
-      const domain = email.split('@')[1];
-      if (domain !== 'example.com') {  // 特定のドメインをチェック
-        return {'invalidDomain': true};
-      }
-    }
-    return null;
-  }
+	domainValidator(control: FormControl): { [key: string]: any } | null {
+		const email = control.value;
+		if (email && email.indexOf("@") !== -1) {
+			const domain = email.split("@")[1];
+			if (domain !== "example.com") {
+				// 特定のドメインをチェック
+				return { invalidDomain: true };
+			}
+		}
+		return null;
+	}
 }
